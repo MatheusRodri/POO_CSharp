eb86ac7c1ed1645a01347b2124609da3c7e0bc15
Matheus <matheus.rj25@hotmail.com>
2023-02-16 19:34:48 -0300

    add project

---
+++ Aula01/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Hello, World!");
+
+String nome = "Matheus Rodrigues";
+char sala = 'B';
+int idade = 20;
+decimal salario = 25.50m;
+float tamanho = 1.15f;
+
+Console.WriteLine("Seu nome: " + nome + " pertecence a sala: " + sala);
+Console.WriteLine($"Seu nome: {nome}, tem {idade} anos e mede {tamanho}, pertecence a sala: {sala} e recebe {salario} por hora");
+
+int valor1 = 1;
+int valor2 = 2;
+
+Console.WriteLine("Soma: " + (valor1 + valor2));
